81deb4e57620b8fbfc94d74670e9f12ce27eaa38
BabuRipon <riponbabu392@gmail.com>
2022-03-12 05:44:28 +0530

    solved error by changing the client to frontend in app.js file

--- app.js
+++ app.js
@@ -24,13 +24,13 @@ mongoose.connect(process.env.DATABASE_URL,{
 // app.use(cors());
 app.use(morgan('combined'))
 app.use(express.json());
-app.use(express.static(path.join(__dirname, "client", "build")))
+app.use(express.static(path.join(__dirname, "frontend", "build")))
 
 
 app.use('/api',msgRoute);
 
 app.get("*", (req, res) => {
-    res.sendFile(path.join(__dirname, "client", "build", "index.html"));
+    res.sendFile(path.join(__dirname, "frontend", "build", "index.html"));
 });
 
 app.listen(port,()=>{
